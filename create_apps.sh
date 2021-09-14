2dc897d455b6c65069614fbda921f5b7f019b429
Jon Worek <jworek@usertesting.com>
2021-09-14 11:01:59 -0400

    Creates first app

--- create_apps.sh
+++ create_apps.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-nativefier "https://calendar.google.com" \
+OUTPUT_DIRECTORY=apps/
+
+nativefier \
+"https://calendar.google.com" \
+$OUTPUT_DIRECTORY  \
 --internal-urls "calendar.google.*?" \
 --single-instance \
---user-agent "edge" 
+--user-agent "edge" \
 --name "Google Calendar"
